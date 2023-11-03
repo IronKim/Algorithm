c66f04b3dcbfeac6fffb56405910bfc4853cd497
IronKim <90168791+IronKim@users.noreply.github.com>
2023-11-03 17:38:45 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] arg){
+        int a, b;
+		Scanner s = new Scanner(System.in);
+		a = s.nextInt();
+		b = s.nextInt();
+		System.out.println(a - b);
+
+
+    }
+}

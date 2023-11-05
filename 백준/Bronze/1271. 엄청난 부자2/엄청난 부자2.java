ad12acc43401919806f13b14ab3ec9fb781ed52e
IronKim <90168791+IronKim@users.noreply.github.com>
2023-11-06 02:40:47 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 232 ms, Memory: 17864 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		
+		BigInteger n = scan.nextBigInteger();
+		BigInteger m = scan.nextBigInteger();
+		System.out.println(n.divide(m));
+		System.out.println(n.mod(m));
+		
+	}
+}

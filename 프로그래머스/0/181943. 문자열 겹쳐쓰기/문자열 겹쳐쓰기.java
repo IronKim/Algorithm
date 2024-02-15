6c3a56b904546fbc0f8bc003a51bceb627c41429
IronKim <90168791+IronKim@users.noreply.github.com>
2024-02-15 16:09:07 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 10.55 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,6 +1,6 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        return my_string.subSequence(0, s).toString() + 
+        return my_string.substring(0, s) + 
             overwrite_string + my_string.substring(overwrite_string.length() + s);
     }
 }

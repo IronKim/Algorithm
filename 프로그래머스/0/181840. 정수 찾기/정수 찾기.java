4d4f7682d516636fa5495a5971ecbab977fccd0f
IronKim <90168791+IronKim@users.noreply.github.com>
2024-02-16 15:49:07 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,7 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] num_list, int n) {
-        return Arrays.stream(num_list).filter(i -> i == n).findFirst().isPresent() ? 1 : 0;
+        
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] == n)
+                return 1;
+        }
+        
+        
+        return 0;
     }
 }

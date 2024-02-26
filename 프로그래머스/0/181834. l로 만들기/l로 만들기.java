dbaa7a2d2e3ac678af9d316a5af64ca997a2666b
IronKim <90168791+IronKim@users.noreply.github.com>
2024-02-26 17:12:16 +0900

    [level 0] Title: l로 만들기, Time: 15.27 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,11 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        StringBuffer sb = new StringBuffer();
-        
-        for(char c : myString.toCharArray()) {
-            sb.append(c > 'l' ? c : 'l');
-        } 
-        
-        return sb.toString();
+        return myString.replaceAll("[^l-z]", "l");
     }
 }

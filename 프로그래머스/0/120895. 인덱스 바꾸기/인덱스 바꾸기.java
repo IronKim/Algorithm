ce06eaf087f38bb189eab3d7cb7bc00c332b133b
IronKim <90168791+IronKim@users.noreply.github.com>
2024-02-27 15:49:24 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,17 +1,13 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
         String answer = "";
-        
-        for(int i = 0; i < my_string.length(); i ++) {
-        	if(i == num1) {
-        		answer += my_string.charAt(num2);
-        	}else if(i == num2) {
-        		answer += my_string.charAt(num1);
-        	}else {
-        		answer += my_string.charAt(i);
-        	}
-        }
-        
+
+        char[] ch = my_string.toCharArray();
+
+        ch[num1] = my_string.charAt(num2);
+        ch[num2] = my_string.charAt(num1);
+
+        answer = String.valueOf(ch);
         return answer;
     }
 }

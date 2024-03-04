4015f1b9df18b7b4b75f622f45951258c926ee73
IronKim <90168791+IronKim@users.noreply.github.com>
2024-03-04 17:22:47 +0900

    [level 0] Title: 약수 구하기, Time: 2.05 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -3,15 +3,13 @@ import java.util.*;
 class Solution {
     public int[] solution(int n) {
         List<Integer> answer = new ArrayList<Integer>();
-        
-        int temp = n;
-        
+             
         for(int i = 1; i <= n; i++) {
         	if(n % i == 0) {
         		answer.add(i);
         	}
         }
         
-        return answer.stream().sorted().mapToInt(i -> i).toArray();
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }

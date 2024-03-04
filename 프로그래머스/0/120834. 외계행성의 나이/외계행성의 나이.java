47c65cbf6be935b0cd2d8bd523f5fef66ecda899
IronKim <90168791+IronKim@users.noreply.github.com>
2024-03-04 18:15:46 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,44 +1,12 @@
 class Solution {
     public String solution(int age) {
-        String answer = "";
-        
-        String ageStr = String.valueOf(age);
-        
-        for(char c : ageStr.toCharArray()) {
-        	switch (c) {
-			case '0':
-				answer += "a";
-				break;
-			case '1':
-				answer += "b";
-				break;
-			case '2':
-				answer += "c";
-				break;
-			case '3':
-				answer += "d";
-				break;
-			case '4':
-				answer += "e";
-				break;
-			case '5':
-				answer += "f";
-				break;
-			case '6':
-				answer += "g";
-				break;
-			case '7':
-				answer += "h";
-				break;
-			case '8':
-				answer += "i";
-				break;
-			case '9':
-				answer += "j";
-				break;
-			}
+        StringBuilder sb = new StringBuilder();
+
+        while(age > 0) {
+            sb.insert(0, (char) ((age % 10) + (int)'a'));
+            age /= 10;
         }
-        
-        return answer;
+
+        return sb.toString();
     }
 }

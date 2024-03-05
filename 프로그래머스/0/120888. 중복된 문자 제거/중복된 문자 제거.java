768dcce431020b578fce0fc893ca3c4dd5d8e1d0
IronKim <90168791+IronKim@users.noreply.github.com>
2024-03-05 20:58:27 +0900

    [level 0] Title: 중복된 문자 제거, Time: 13.34 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(char c : my_string.toCharArray()) {
+        	if(!answer.contains(String.valueOf(c))) {
+        		answer += c;
+        	}
+        }
+        
+        return answer;
+    }
+}

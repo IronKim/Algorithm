f176c92d031a59ed60c5b2c1360407f13276898d
IronKim <90168791+IronKim@users.noreply.github.com>
2024-03-06 18:05:43 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 3.81 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = Arrays.copyOf(arr, arr.length);
+        
+        for(int i = 0; i < answer.length; i++) {
+        	for(int j = 0; j < queries.length; j++) {
+        		if(queries[j][0] <= i && i <= queries[j][1]) {
+        			answer[i]++;
+        		}
+        	}
+        }
+        
+        return answer;
+    }
+}

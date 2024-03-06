d7f6097cdabaac84b38a5be79c6329e0ded657fa
IronKim <90168791+IronKim@users.noreply.github.com>
2024-03-06 18:31:33 +0900

    [level 0] Title: 글자 지우기, Time: 0.06 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        char[] answer = my_string.toCharArray();
+        
+        for(int i = 0; i < indices.length; i++) {
+        	answer[indices[i]] = '`';
+        }
+        
+        return String.valueOf(answer).replace("`", "");
+    }
+}

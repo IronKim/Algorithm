994ab3c4943667828096e7d669ab69d28fabc203
IronKim <90168791+IronKim@users.noreply.github.com>
2024-03-06 19:04:14 +0900

    [level 0] Title: 문자열 뒤집기, Time: 14.60 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,19 +1,7 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
-        String answer = "";
-        StringBuilder sb = new StringBuilder(my_string.substring(s, e + 1));
-        sb.reverse();
-        
-        for(int i = 0; i < my_string.length(); i++) {
-        	if(s <= i && i <= e) {
-        		answer += sb.charAt(i - s);
-        	}else {
-                answer += my_string.charAt(i);
-            }
-        	
-        	
-        }
-        
-        return answer;
+        StringBuilder answer = new StringBuilder(my_string.substring(s, e + 1));
+        answer.reverse();
+        return my_string.substring(0, s) + answer + my_string.substring(e + 1);
     }
 }

8e71fb11a077996c42fe2425ae868467f18f182c
IronKim <90168791+IronKim@users.noreply.github.com>
2024-03-06 17:50:40 +0900

    [level 0] Title: 날짜 비교하기, Time: 1.06 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,10 +1,10 @@
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        LocalDateTime localDateTime1 = LocalDateTime.of(date1[0], date1[1], date1[2], 0, 0);
-        LocalDateTime localDateTime2 = LocalDateTime.of(date2[0], date2[1], date2[2], 0, 0);
+        LocalDate localDate1 = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate localDate2 = LocalDate.of(date2[0], date2[1], date2[2]);
         
-        return localDateTime1.isBefore(localDateTime2) ? 1 : 0;
+        return localDate1.isBefore(localDate2) ? 1 : 0;
     }
 }

5096b69eefa2f6d50f0b00398433749d05cad027
IronKim <90168791+IronKim@users.noreply.github.com>
2024-03-22 15:54:19 +0900

    [level 0] Title: 등수 매기기, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int[][] score) {
+        int[] answer = new int[score.length];
+        double[] avg = new double[score.length];
+        
+        for(int i = 0; i < avg.length; i++) {
+        	avg[i] = (score[i][0] + score[i][1]) / 2.; 
+        }
+        
+        for(int i = 0; i < answer.length; i++) {
+        	int temp = 1;
+        	for(int j = 0; j < avg.length; j++) {
+        		if(avg[i] < avg[j]) {
+        			temp++;
+        		}
+        	}
+        	answer[i] = temp;
+        }
+        
+        return answer;
+    }
+}

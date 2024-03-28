cbf7c8fe311a86e1c7ccdad23630fe7274c54e1f
IronKim <90168791+IronKim@users.noreply.github.com>
2024-03-28 17:31:11 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,23 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int num, int total) {
-        List<Integer> answer = new ArrayList<Integer>();
-        
-        for(int i = -500; i < Integer.MAX_VALUE; i++) {
-        	int hap = 0;
-        	for(int j = 0; j < num; j++) {
-        		hap += i + j;
-        	}
-        	if(hap == total) {
-        		for(int j = 0; j < num; j++) {
-            		answer.add(i + j);
-            	}
-        		break;
-        	}
-        	
+        int[] answer = new int[num];
+        int check = num*(num+1) / 2;
+        int start = (total - check) / num + 1;
+        for (int i = 0; i < answer.length; i++) {
+                answer[i] = start + i ;
         }
-      
-        return answer.stream().mapToInt(Integer::valueOf).toArray();
+        return answer;
     }
 }

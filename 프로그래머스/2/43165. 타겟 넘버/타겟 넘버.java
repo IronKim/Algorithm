d98e30928a4a94ef30a21a5aa809f20aaaa681fb
IronKim <90168791+IronKim@users.noreply.github.com>
2024-04-26 16:45:59 +0900

    [level 2] Title: 타겟 넘버, Time: 3.72 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,33 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] numbers, int target) {
-        Stack<State> stack = new Stack<State>();
-        stack.push(new State(0, 0));
-        
         int answer = 0;
-        
-        while(!stack.isEmpty()) {
-            State state = stack.pop();
-            
-            if(state.idx == numbers.length) {
-                if(state.acc == target) answer++;
-                continue;
-            }
-            
-            stack.push(new State(state.idx + 1, state.acc - numbers[state.idx]));
-            stack.push(new State(state.idx + 1, state.acc + numbers[state.idx]));
-        }
-        
         return answer;
-    }
-    class State {
-        int idx;
-        int acc;
-        
-        State(int idx, int acc) {
-            this.idx = idx;
-            this.acc = acc;
-        }
     }
 }

fd73e2caf9847dadb14457090207c3825af87164
IronKim <90168791+IronKim@users.noreply.github.com>
2024-05-13 00:11:40 +0900

    [Silver IV] Title: 듣보잡, Time: 428 ms, Memory: 28960 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.Map.Entry;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<String, Integer> map = new HashMap<String, Integer>();
+		List<String> list = new ArrayList<String>();
+        
+        int a = Integer.valueOf(st.nextToken());
+		int b = Integer.valueOf(st.nextToken());
+        
+        for(int i = 0; i < a + b; i++) {
+			String name = br.readLine();
+			map.put(name, map.getOrDefault(name, 0) + 1);
+		}
+		
+		for (Entry<String, Integer> m : map.entrySet()) {
+			if(m.getValue() == 2) {
+				list.add(m.getKey());
+			}
+		}
+		
+		Collections.sort(list);
+		
+		System.out.println(list.size());
+		
+		for (String string : list) {
+			System.out.println(string);
+		}
+    }
+}

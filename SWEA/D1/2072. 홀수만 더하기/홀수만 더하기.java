fc934eb4876ab3a49c8fe42281d462a134c0ea96
IronKim <90168791+IronKim@users.noreply.github.com>
2024-05-15 10:41:58 +0900

    [D1] Title: 홀수만 더하기, Time: 135 ms, Memory: 26,096 KB -BaekjoonHub

---
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+        sc.nextLine();
+
+		for(int test_case = 1; test_case <= T; test_case++) {
+			String[] str = sc.nextLine().split(" ");
+			int sum = 0;
+			for (String string : str) {
+				int tmp = Integer.valueOf(string);
+				if(tmp % 2 == 1) {
+					sum += tmp;
+				}
+			}
+			System.out.println("#" + test_case  + " " + sum);
+		}
+	}
+}
